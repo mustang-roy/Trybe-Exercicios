a000b6cdb060b98cc786d52d5252459beab8f67d
Luiz F S Mariz <lfsmariz@gmail.com>
2020-08-30 20:38:37 -0300

    Q4 and Q5 Solved

--- bloco_4/4_3/scripts.js
+++ bloco_4/4_3/scripts.js
@@ -32,4 +32,38 @@ function quest03(number) {
   }
 }
 
-quest03(5)
+function quest04(number) {
+
+  let voidSpace = Math.ceil(number/2) - 1;
+  
+  for (let countline = 0; countline < number/2; countline+=1) {
+    let line = "";
+    let voidSpace = Math.ceil((number-1)/2) - countline;
+    for (let countcolumn = 0; countcolumn < (number/2)+countline; countcolumn+=1) {
+        countcolumn >= voidSpace ? line+="*" : line+=" "
+    }    
+    console.log(line);
+  }
+}
+
+function quest05(number) {
+
+  let edgeRight = Math.ceil((number-1)/2);
+  let edgeLeft = Math.floor((number-1)/2);
+  
+  for (let countline = 0; countline < (number/2); countline+=1) {
+    let line = "";
+    for (let countcolumn = 0; countcolumn < (number/2)+countline; countcolumn+=1) {
+        if(countcolumn === edgeLeft || countcolumn === edgeRight || countline === Math.floor(number/2)) {
+          line+="*";
+        } else {
+          line+=" ";
+        }
+    }    
+    edgeLeft -= 1;
+    edgeRight += 1;
+    console.log(line);
+  }
+}
+
+quest05(7)
