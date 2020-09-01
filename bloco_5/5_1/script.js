82ce2afa21086bdeb9cc8398edf7200aa09266ec
Luiz F S Mariz <lfsmariz@gmail.com>
2020-09-01 13:38:47 -0300

    solved Q6

--- bloco_5/5_1/script.js
+++ bloco_5/5_1/script.js
@@ -2,6 +2,17 @@ function changeTagText(tag, text, position=0) {
   document.getElementsByTagName(tag)[position].innerHTML = text;
 }
 
+function capitalizeTagText(tag, position=0) {
+  document.getElementsByTagName(tag)[position].innerHTML = document.getElementsByTagName(tag)[position].innerHTML.toUpperCase() ;
+}
+
+function plotConsoleTag(tag) {
+  let elements = document.getElementsByTagName(tag)
+  for (let item in elements){
+    console.log(elements[item].innerHTML) 
+  }
+}
+
 function changeClassBg(classNm, colorBg, position = 0) {
   document.getElementsByClassName(classNm)[position].style.backgroundColor = colorBg
 }
@@ -14,5 +25,6 @@ changeClassBg("center-content", "white")
 // Questão 4
 changeTagText("h1", "Exercício 5.1 - JavaScript")
 // Questão 5
-
+capitalizeTagText("p")
 // Questão 6
+plotConsoleTag("p")
