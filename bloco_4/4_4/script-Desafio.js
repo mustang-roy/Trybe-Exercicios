582e5c5d65de691651e0f23598d9a4d1ab9a6127
Luiz F S Mariz <lfsmariz@gmail.com>
2020-09-01 13:42:29 -0300

    update script desafio

--- bloco_4/4_4/script-Desafio.js
+++ bloco_4/4_4/script-Desafio.js
@@ -9,15 +9,15 @@ function convertRoman(roman) {
     M: 1000,
   }
 
-  initial = romNumber[roman[0]]
+  let initial = 0;
 
-  for (let index = 1; index < roman.length; index++) {
-    if (romNumber[roman[index]] <= romNumber[roman[index-1]]){
-      initial += romNumber[roman[index]]
+  for (let index = 0; index < roman.length; index++) {
+    if (romNumber[roman[index]] < romNumber[roman[index+1]]){
+      initial -= romNumber[roman[index]]
     } else {
-      initial += romNumber[roman[index]] - 2*romNumber[roman[index-1]]
+      initial += romNumber[roman[index]]
     }    
-  }
+  } 
 
   console.log(initial)
 }
