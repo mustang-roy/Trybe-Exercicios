1b1c17c39561848d7d01834245d291c41cbcd782
Luiz F S Mariz <lfsmariz@gmail.com>
2020-09-01 13:31:57 -0300

    solved Q4

--- bloco_5/5_1/script.js
+++ bloco_5/5_1/script.js
@@ -1,4 +1,4 @@
-function changeTagText(tag, text, position,) {
+function changeTagText(tag, text, position=0) {
   document.getElementsByTagName(tag)[position].innerHTML = text;
 }
 
@@ -6,13 +6,13 @@ function changeClassBg(classNm, colorBg, position = 0) {
   document.getElementsByClassName(classNm)[position].style.backgroundColor = colorBg
 }
 // Questão 1
-changeTagText("p", "programando muito, muito mesmo", 1)
+changeTagText("p", "programando muito, muito mesmo")
 // Questão 2
 changeClassBg("main-content", "rgb(76,164,109)")
 // Questão 3
 changeClassBg("center-content", "white")
 // Questão 4
-
+changeTagText("h1", "Exercício 5.1 - JavaScript")
 // Questão 5
 
 // Questão 6
