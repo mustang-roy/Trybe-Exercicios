ed362dbe174b617faedb17279577283f6e598504
Luiz F S Mariz <lfsmariz@gmail.com>
2020-09-04 10:13:09 -0300

    solved exercises

--- bloco_5/5_2/script.js
+++ bloco_5/5_2/script.js
@@ -14,3 +14,48 @@ divMain.appendChild(divCenter)
 let paragraphText = document.createElement('p')
 paragraphText.innerHTML = 'bolinha'
 divCenter.appendChild(paragraphText)
+
+let leftContent = document.createElement('div')
+leftContent.className='left-content'
+divMain.appendChild(leftContent)
+
+let rightContent = document.createElement('div')
+rightContent.className='right-content'
+divMain.appendChild(rightContent)
+
+let imgLeft = document.createElement('img')
+imgLeft.src='https://picsum.photos/200'
+imgLeft.className = 'small-image'
+leftContent.appendChild(imgLeft)
+
+let listRight = document.createElement('ul')
+
+for (let index = 0; index < 10; index += 1) {
+  let itemList = document.createElement('li')
+  itemList.innerHTML = index+1
+  listRight.appendChild(itemList)
+}
+
+rightContent.appendChild(listRight)
+
+for (let index = 0; index < 3; index +=1) {
+  let title3 = document.createElement('h3')
+  title3.innerHTML = `TITULO ${index + 1}`
+  title3.className = 'description'
+  
+  divMain.appendChild(title3)  
+}
+
+titlesPage.className = 'title'
+
+divMain.removeChild(leftContent)
+
+rightContent.style.marginRight = 'auto'
+
+divCenter.style.backgroundColor = 'green'
+
+listaItensTotal = document.getElementsByTagName('ul')[0]
+
+for (let index = 0; index < 2; index++) {
+  listaItensTotal.removeChild(listaItensTotal.lastChild)
+}
