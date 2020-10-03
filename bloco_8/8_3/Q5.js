6f60d12e1b5994c69c6e30154440a2c4b0d87238
Luiz F S Mariz <lfsmariz@gmail.com>
2020-10-03 12:31:50 -0300

    8_3

---
+++ bloco_8/8_3/Q5.js
@@ -0,0 +1,23 @@
+    
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+
+
+function containsA() {
+ let value = names.reduce((acc, curr) => {
+    return acc + curr.split('').reduce((acc2, curr2) => {
+      if (curr2.toLowerCase() == 'a') return acc2 + 1
+      return acc2
+    }, 0)
+  },0)
+
+  return value
+}
+
+assert.deepEqual(containsA(), 20);
